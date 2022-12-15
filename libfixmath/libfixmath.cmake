b2d368f7cada3fb125c4e5400aaf52dd75ec42f7
Danilo Spinella <oss@danyspin97.org>
2022-12-15 16:58:40 +0100

    Install headers and library

--- libfixmath/libfixmath.cmake
+++ libfixmath/libfixmath.cmake
@@ -4,3 +4,10 @@ add_library(libfixmath STATIC ${libfixmath-srcs})
 
 target_include_directories(libfixmath INTERFACE
   ${CMAKE_CURRENT_SOURCE_DIR})
+
+install(TARGETS fixmath
+        DESTINATION ${CMAKE_INSTALL_LIBDIR})
+
+file(GLOB libfixmath-headers libfixmath/*.h libfixmath/*.hpp)
+install(FILES ${libfixmath-headers}
+        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/libfixmath)
